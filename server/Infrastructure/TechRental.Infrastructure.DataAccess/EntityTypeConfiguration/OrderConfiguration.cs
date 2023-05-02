bdd7146acf64a147069045a651158f0434074355
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-05-02 21:22:17 +0300

    feat: add entity type configurations

---
+++ server/Infrastructure/TechRental.Infrastructure.DataAccess/EntityTypeConfiguration/OrderConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using TechRental.Domain.Core.Orders;
+using TechRental.Infrastructure.DataAccess.ValueConverters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TechRental.Infrastructure.DataAccess.EntityTypeConfiguration;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.OrderDate);
+        builder.Property(x => x.TotalPrice);
+
+        builder.Property(x => x.Status).HasConversion<OrderStatusValueConverter>();
+
+        builder.HasOne(x => x.User)
+            .WithMany(x => x.Orders)
+            .HasForeignKey(x => x.UserId);
+    }
+}
---
+++ server/Infrastructure/TechRental.Infrastructure.DataAccess/EntityTypeConfiguration/UserConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using TechRental.Domain.Core.Users;
+using TechRental.Infrastructure.DataAccess.ValueConverters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TechRental.Infrastructure.DataAccess.EntityTypeConfiguration;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.LastName);
+        builder.Property(x => x.FirstName);
+        builder.Property(x => x.MiddleName);
+        builder.Property(x => x.BirthDate);
+
+        builder.Property(x => x.Image).HasConversion<ImageValueConverter>();
+        builder.Property(x => x.PhoneNumber).HasConversion<PhoneNumberValueConverter>();
+
+        builder.Navigation(x => x.Orders).HasField("_orders");
+    }
+}
