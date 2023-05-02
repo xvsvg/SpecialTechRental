4aca744c9db5c103a2f04830eaee45a684bc71db
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-05-02 21:23:27 +0300

    feat: add database dependency injection

---
+++ server/Infrastructure/TechRental.Infrastructure.DataAccess/Extensions/RegistrationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using TechRental.DataAccess.Abstractions;
+using TechRental.Infrastructure.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TechRental.Infrastructure.DataAccess.Extensions;
+
+public static class RegistrationExtensions
+{
+    public static IServiceCollection AddDatabaseContext(
+        this IServiceCollection collection,
+        Action<DbContextOptionsBuilder> action)
+    {
+        collection.AddDbContext<IDatabaseContext, DatabaseContext>(action);
+
+        return collection;
+    }
+
+    public static Task UseDatabaseContext(this IServiceProvider provider)
+    {
+        DatabaseContext context = provider.GetRequiredService<DatabaseContext>();
+
+        return context.Database.MigrateAsync();
+    }
+}
