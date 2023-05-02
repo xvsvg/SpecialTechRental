0c4543f4d0536e294515ba4e63faf4a7576a906b
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-05-02 16:10:50 +0300

    feat: add domain entities

---
+++ server/Domain/TechRental.Domain.Core/Orders/Order.cs
@@ -0,0 +1,38 @@
+﻿using TechRental.Domain.Common.Exceptions;
+using TechRental.Domain.Core.Abstractions;
+
+namespace TechRental.Domain.Core.Orders;
+
+public class Order
+{
+    private decimal _totalPrice;
+
+    public Order(
+        Guid id,
+        OrderStatus status,
+        decimal total,
+        DateTime orderDate)
+    {
+        Id = id;
+        Status = status;
+        TotalPrice = total;
+        OrderDate = orderDate;
+    }
+
+    public Guid Id { get; }
+    public OrderStatus Status { get; }
+    public DateTime OrderDate { get; }
+
+    public decimal TotalPrice
+    {
+        get => _totalPrice;
+
+        set
+        {
+            if (value < 0)
+                throw UserInputException.NegativeOrderTotalException();
+
+            _totalPrice = value;
+        }
+    }
+}
