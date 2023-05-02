b48d0d6b65ac4b5af5f45ec26094a0155180165c
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-05-02 16:10:30 +0300

    feat: add domain value object

---
+++ server/Domain/TechRental.Domain.Core/ValueObject/PhoneNumber.cs
@@ -0,0 +1,16 @@
+﻿using TechRental.Domain.Common.Exceptions;
+
+namespace TechRental.Domain.Core.ValueObject;
+
+public readonly record struct PhoneNumber
+{
+    public PhoneNumber(string number)
+    {
+        if (string.IsNullOrEmpty(number) is true)
+            throw UserInputException.InvalidPhoneNumberException($"{number} is not in correct format.");
+
+        Value = number;
+    }
+
+    public string Value { get; }
+}
