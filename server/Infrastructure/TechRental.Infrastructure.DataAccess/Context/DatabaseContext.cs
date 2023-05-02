383e434da3ae41517e7ac8eb327979fd3f07bc46
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-05-02 21:21:14 +0300

    feat: implement database context

---
+++ server/Infrastructure/TechRental.Infrastructure.DataAccess/Context/DatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using TechRental.DataAccess.Abstractions;
+using TechRental.Domain.Core.Orders;
+using TechRental.Domain.Core.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace TechRental.Infrastructure.DataAccess.Context;
+
+public class DatabaseContext : DbContext, IDatabaseContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<Order> Orders { get; protected init; } = null!;
+    public DbSet<User> Users { get; protected init; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(IAssemblyMarker).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
