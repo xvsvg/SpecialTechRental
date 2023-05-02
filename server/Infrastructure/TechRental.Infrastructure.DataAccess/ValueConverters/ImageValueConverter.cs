ddcb330f9aef2505baf31c4b2609cb8d39e84fc9
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-05-02 21:22:03 +0300

    feat: add value converters

---
+++ server/Infrastructure/TechRental.Infrastructure.DataAccess/ValueConverters/ImageValueConverter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using TechRental.Domain.Core.Abstractions;
+
+namespace TechRental.Infrastructure.DataAccess.ValueConverters;
+
+public class ImageValueConverter : ValueConverter<UserImage, string>
+{
+    public ImageValueConverter()
+        : base(
+            x => x.ToString() ?? string.Empty,
+            x => new UserImage(Convert.FromBase64String(x)))
+    {
+    }
+}
