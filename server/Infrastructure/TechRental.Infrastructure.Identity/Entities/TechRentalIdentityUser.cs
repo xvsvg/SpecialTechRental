c26bac036fb0dbba5f13158f49c32bdad75e4e60
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-05-03 22:27:59 +0300

    feat: add identity user

---
+++ server/Infrastructure/TechRental.Infrastructure.Identity/Entities/TechRentalIdentityUser.cs
@@ -0,0 +1,12 @@
+﻿using TechRental.Application.Dto.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace TechRental.Infrastructure.Identity.Entities;
+
+internal class TechRentalIdentityUser : IdentityUser<Guid>
+{
+    public IdentityUserDto ToDto()
+    {
+        return new IdentityUserDto(Id, UserName ?? string.Empty);
+    }
+}
