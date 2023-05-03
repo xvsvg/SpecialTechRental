eab6843982a134282b7a5c799dfd8d13c7e14d0b
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-05-03 19:47:45 +0300

    feat: add mappings

---
+++ server/Infrastructure/TechRental.Infrastructure.Mapping/Users/UserMapping.cs
@@ -0,0 +1,21 @@
+﻿using TechRental.Application.Dto.Users;
+using TechRental.Domain.Core.Users;
+using TechRental.Infrastructure.Mapping.Orders;
+
+namespace TechRental.Infrastructure.Mapping.Users;
+
+public static class UserMapping
+{
+    public static UserDto ToDto(this User user)
+    {
+        return new UserDto(
+            user.Id,
+            user.FirstName,
+            user.MiddleName,
+            user.LastName,
+            user.Image.Value,
+            user.BirthDate,
+            user.PhoneNumber.Value,
+            user.Orders.ToDto());
+    }
+}
