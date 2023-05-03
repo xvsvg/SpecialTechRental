26a2bf33f55131a13d687db40e14d45a60ca2098
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-05-03 22:29:01 +0300

    feat: add identity configuration

---
+++ server/Infrastructure/TechRental.Infrastructure.Identity/Tools/IdentityConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace TechRental.Infrastructure.Identity.Tools;
+
+public class IdentityConfiguration
+{
+    public string Secret { get; init; } = string.Empty;
+
+    public string Issuer { get; init; } = string.Empty;
+
+    public string Audience { get; init; } = string.Empty;
+
+    public int ExpiresHours { get; init; }
+}
