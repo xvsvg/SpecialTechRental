8e9d55339bb992f996c525f1226b70764c904fe0
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-05-03 19:47:31 +0300

    feat: add dto

---
+++ server/Application/TechRental.Application.Dto/Orders/OrderDto.cs
@@ -0,0 +1,10 @@
+﻿using TechRental.Application.Dto.Users;
+
+namespace TechRental.Application.Dto.Orders;
+
+public record OrderDto(
+    Guid Id,
+    UserDto User,
+    string Status,
+    decimal Total,
+    DateTime OrderDate);
