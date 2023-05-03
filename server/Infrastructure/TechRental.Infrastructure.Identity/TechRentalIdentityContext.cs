9ffa2407eb8e63254f7e8963725dacc846e9cd2b
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-05-04 00:00:39 +0300

    feat: add identity context

---
+++ server/Infrastructure/TechRental.Infrastructure.Identity/TechRentalIdentityContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TechRental.Infrastructure.Identity.Entities;
+
+namespace TechRental.Infrastructure.Identity;
+
+internal sealed class TechRentalIdentityContext : IdentityDbContext<TechRentalIdentityUser, TechRentalIdentityRole, Guid>
+{
+    public TechRentalIdentityContext(DbContextOptions<TechRentalIdentityContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();
+    }
+}
