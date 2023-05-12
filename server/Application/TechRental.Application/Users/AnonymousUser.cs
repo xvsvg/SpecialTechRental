a76a48c5dc083cd3609fa302831a8824ea4c166a
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-12 21:48:34 +0300

    feat: add application users

---
+++ server/Application/TechRental.Application/Users/AnonymousUser.cs
@@ -0,0 +1,20 @@
+﻿using TechRental.Application.Abstractions.Identity;
+using TechRental.Application.Common.Exceptions;
+using TechRental.Domain.Common.Exceptions;
+
+namespace TechRental.Application.Users;
+
+internal class AnonymousUser : ICurrentUser
+{
+    public Guid Id => throw new UnauthorizedException("Attempt to access anonymous user id");
+
+    public bool CanCreateUserWithRole(string roleName)
+        => throw UserHasNotAccessException.AnonymousUserHasNotAccess();
+
+    public bool CanChangeUserRole(string currentRoleName, string newRoleName)
+        => throw UserHasNotAccessException.AnonymousUserHasNotAccess();
+
+
+    public bool CanChangeOrderStatus()
+        => throw UserHasNotAccessException.AnonymousUserHasNotAccess();
+}
