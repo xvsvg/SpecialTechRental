6de0e2e13904b81d3d39cc86e4a39cf395b82fd5
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-12 21:49:10 +0300

    feat: add application exceptions

---
+++ server/Application/TechRental.Application.Common/Exceptions/AccessDeniedException.cs
@@ -0,0 +1,9 @@
+﻿namespace TechRental.Application.Common.Exceptions;
+
+public class AccessDeniedException : ApplicationException
+{
+    public AccessDeniedException() : base("Access denied") { }
+
+    public AccessDeniedException(string message) : base(message) { }
+
+}
---
+++ server/Application/TechRental.Application.Common/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace TechRental.Application.Common.Exceptions;
+
+public class EntityNotFoundException : ApplicationException
+{
+    public EntityNotFoundException(string message) : base(message) { }
+
+    public static EntityNotFoundException For<T>(Guid id)
+        => new EntityNotFoundException($"Entity of type {typeof(T).Name} with id {id} was not found");
+}
