0dfd958ecf3bfbe455cf6e7c2cd7c8af683fd98d
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-12 21:48:13 +0300

    feat: add application DI

---
+++ server/Application/TechRental.Application/Extensions/RegistrationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TechRental.Application.Abstractions.Identity;
+using TechRental.Application.Handlers.Extensions;
+using TechRental.Application.Users;
+
+namespace TechRental.Application.Extensions;
+
+public static class RegistrationExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection collection)
+    {
+        collection.AddHandlers();
+        collection.AddCurrentUser();
+
+        return collection;
+    }
+
+    private static void AddCurrentUser(this IServiceCollection collection)
+    {
+        collection.AddScoped<CurrentUserProxy>();
+        collection.AddScoped<ICurrentUser>(x => x.GetRequiredService<CurrentUserProxy>());
+        collection.AddScoped<ICurrentUserManager>(x => x.GetRequiredService<CurrentUserProxy>());
+    }
+}
