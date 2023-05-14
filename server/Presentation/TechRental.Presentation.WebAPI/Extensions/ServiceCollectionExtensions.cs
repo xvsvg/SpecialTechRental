cafd6957ffa06c5744aa562609fd6d04caed8804
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-14 23:23:21 +0300

    feat: add web api extensions

---
+++ server/Presentation/TechRental.Presentation.WebAPI/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechRental.Application.Extensions;
+using TechRental.Infrastructure.DataAccess.Extensions;
+using TechRental.Infrastructure.Identity.Extensions;
+using TechRental.Presentation.Controllers;
+using TechRental.Presentation.WebAPI.Configuration;
+
+namespace TechRental.Presentation.WebAPI.Extensions;
+
+internal static class ServiceCollectionExtensions
+{
+    internal static IServiceCollection ConfigureServices(
+        this IServiceCollection collection,
+        WebApiConfiguration configuration,
+        IConfigurationSection identityConfigurationSection)
+    {
+        collection
+            .AddControllers()
+            .AddApplicationPart(typeof(IControllerProjectMarker).Assembly)
+            .AddControllersAsServices();
+
+        string connectionString = configuration.PostgresConfiguration
+            .ToConnectionString(configuration.DbNamesConfiguration.ApplicationDbName);
+
+        collection
+            .AddEndpointsApiExplorer()
+            .AddSwaggerGen()
+            .AddApplication()
+            .AddDatabaseContext(o => o
+                .UseNpgsql(connectionString));
+
+        connectionString = configuration.PostgresConfiguration
+            .ToConnectionString(configuration.DbNamesConfiguration.IdentityDbName);
+
+        collection.AddIdentityConfiguration(
+            identityConfigurationSection,
+            o => o.UseNpgsql(
+                configuration.PostgresConfiguration.ToConnectionString(connectionString)));
+
+        return collection;
+    }
+}
---
+++ server/Presentation/TechRental.Presentation.WebAPI/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace TechRental.Presentation.WebAPI.Extensions;
+
+internal static class WebApplicationExtensions
+{
+    internal static WebApplication Configure(this WebApplication app)
+    {
+        app
+            .UseSwagger()
+            .UseSwaggerUI();
+
+        app
+            .UseRouting()
+            .UseAuthentication()
+            .UseAuthorization();
+
+        app.UseCors(o => o.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+        app.MapControllers();
+
+        return app;
+    }
+}
