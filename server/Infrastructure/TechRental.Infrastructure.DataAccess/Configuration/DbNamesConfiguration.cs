d76b172828cea93bc3596c8338381a8f6276816b
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-14 23:20:44 +0300

    feat: add db names configuration

---
+++ server/Infrastructure/TechRental.Infrastructure.DataAccess/Configuration/DbNamesConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace TechRental.Infrastructure.DataAccess.Configuration;
+
+public class DbNamesConfiguration
+{
+    public string ApplicationDbName { get; set; } = string.Empty;
+    public string IdentityDbName { get; set; } = string.Empty;
+}
