cf621f67369597f08632f9ac4bf6a1d0a37b4966
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-14 23:22:51 +0300

    feat: add web api configuration

---
+++ server/Presentation/TechRental.Presentation.WebAPI/Configuration/WebApiConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using TechRental.Infrastructure.DataAccess.Configuration;
+
+namespace TechRental.Presentation.WebAPI.Configuration;
+
+internal class WebApiConfiguration
+{
+    public WebApiConfiguration(IConfiguration configuration)
+    {
+        if (configuration is null)
+            throw new ArgumentNullException(nameof(configuration));
+
+        var postgresConfiguration = configuration
+            .GetSection(nameof(PostgresConfiguration))
+            .Get<PostgresConfiguration>();
+
+        PostgresConfiguration = postgresConfiguration ??
+                                throw new ArgumentException(nameof(PostgresConfiguration));
+
+        var dbNameConfiguration = configuration
+            .GetSection(nameof(DbNamesConfiguration))
+            .Get<DbNamesConfiguration>();
+
+        DbNamesConfiguration = dbNameConfiguration ??
+                               throw new ArgumentException(nameof(DbNamesConfiguration));
+    }
+
+    public PostgresConfiguration PostgresConfiguration { get; set; }
+    public DbNamesConfiguration DbNamesConfiguration { get; set; }
+}
