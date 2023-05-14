c54190b603d4f4b719984454e402e97067baebba
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-14 23:27:09 +0300

    feat: modify entrypoint

--- server/Presentation/TechRental.Presentation.WebAPI/Program.cs
+++ server/Presentation/TechRental.Presentation.WebAPI/Program.cs
@@ -1,6 +1,23 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using TechRental.Presentation.WebAPI.Configuration;
+using TechRental.Presentation.WebAPI.Extensions;
+using TechRental.Presentation.WebAPI.Helpers;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilogForAppLogs(builder.Configuration);
+
+var webApiConfiguration = new WebApiConfiguration(builder.Configuration);
+var identityConfigurationSection = builder.Configuration.GetSection("Identity:IdentityConfiguration");
+
+builder.Services.ConfigureServices(
+    webApiConfiguration,
+    identityConfigurationSection);
+
+var app = builder.Build().Configure();
+
+using (var scope = app.Services.CreateScope())
+{
+    await SeedingHelper.SeedAdmins(scope.ServiceProvider, app.Configuration);
+}
 
 app.Run();
