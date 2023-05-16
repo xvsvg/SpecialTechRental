9da2fc4755ce9b8d3efaaf3519a83b3282319e3d
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-16 23:02:47 +0300

    feat: add get all users response model

---
+++ server/Presentation/TechRental.Presentation.Contracts/Users/GetAllUsersResponse.cs
@@ -0,0 +1,5 @@
+﻿using TechRental.Application.Dto.Users;
+
+namespace TechRental.Presentation.Contracts.Users;
+
+public record GetAllUsersResponse(IEnumerable<UserDto> Users, int Page, int TotalPages);
