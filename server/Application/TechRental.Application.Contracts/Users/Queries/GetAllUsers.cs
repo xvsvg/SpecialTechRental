8901673cda0fea4c7febc5e944eaa5878d8f2d00
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-16 23:00:55 +0300

    feat: change get all users contract configuration

--- server/Application/TechRental.Application.Contracts/Users/Queries/GetAllUsers.cs
+++ server/Application/TechRental.Application.Contracts/Users/Queries/GetAllUsers.cs
@@ -5,7 +5,7 @@ namespace TechRental.Application.Contracts.Users.Queries;
 
 internal class GetAllUsers
 {
-    public record Query() : IRequest<Response>;
+    public record Query(int Page) : IRequest<Response>;
 
-    public record Response(IEnumerable<UserDto> Users);
+    public record Response(IEnumerable<UserDto> Users, int Page, int TotalPages);
 }
