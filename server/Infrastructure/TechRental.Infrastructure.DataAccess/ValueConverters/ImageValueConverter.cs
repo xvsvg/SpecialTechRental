8e7ae443b29e4ad08de88f7c92815c7042845573
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-16 20:33:19 +0300

    fix: naming

--- server/Infrastructure/TechRental.Infrastructure.DataAccess/ValueConverters/ImageValueConverter.cs
+++ server/Infrastructure/TechRental.Infrastructure.DataAccess/ValueConverters/ImageValueConverter.cs
@@ -3,12 +3,12 @@ using TechRental.Domain.Core.Abstractions;
 
 namespace TechRental.Infrastructure.DataAccess.ValueConverters;
 
-public class ImageValueConverter : ValueConverter<UserImage, string>
+public class ImageValueConverter : ValueConverter<Image, string>
 {
     public ImageValueConverter()
         : base(
             x => (x.Value.LongLength == 0L ? string.Empty : x.Value.ToString()) ?? string.Empty,
-            x => new UserImage(Convert.FromBase64String(x)))
+            x => new Image(Convert.FromBase64String(x)))
     {
     }
 }
