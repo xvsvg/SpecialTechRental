0142438c8fa37fab7ff55d4b9fd4f080078012fb
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-16 20:14:41 +0300

    feat: change create user contract

--- server/Application/TechRental.Application.Contracts/Identity/Commands/CreateUserAccount.cs
+++ server/Application/TechRental.Application.Contracts/Identity/Commands/CreateUserAccount.cs
@@ -4,5 +4,7 @@ namespace TechRental.Application.Contracts.Identity.Commands;
 
 internal static class CreateUserAccount
 {
-    public record Command(Guid UserId, string Username, string Password, string RoleName) : IRequest;
+    public record Command(string Username, string Password, string RoleName) : IRequest<Response>;
+
+    public record Response(string Token);
 }
