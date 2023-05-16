b14ea458cbd1c1d9ffa90fbc0d33d24d4901ce85
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-16 20:37:49 +0300

    feat: apply restrictions to create user request

--- server/Presentation/TechRental.Presentation.Contracts/Users/CreateUserRequest.cs
+++ server/Presentation/TechRental.Presentation.Contracts/Users/CreateUserRequest.cs
@@ -1,9 +1,9 @@
 ﻿namespace TechRental.Presentation.Contracts.Users;
 
 public record CreateUserRequest(
-    string? FirstName,
-    string? MiddleName,
-    string? LastName,
-    string? PhoneNumber,
+    string FirstName,
+    string MiddleName,
+    string LastName,
+    string PhoneNumber,
     string? UserImage,
     DateTime BirthDate);
