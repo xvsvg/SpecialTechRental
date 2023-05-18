f9d7867d97687d51a13addf5b47c07a0a8ccd9dc
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-18 12:27:53 +0300

    feat: add authorize admin handler

---
+++ server/Application/TechRental.Application.Handlers/Identity/AuthorizeAdminHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using TechRental.Application.Abstractions.Identity;
+using static TechRental.Application.Contracts.Identity.Queries.AuthorizeAdmin;
+
+namespace TechRental.Application.Handlers.Identity;
+
+internal class AuthorizeAdminHandler : IRequestHandler<Query>
+{
+    private readonly IAuthorizationService _authorizationService;
+
+    public AuthorizeAdminHandler(IAuthorizationService authorizationService)
+    {
+        _authorizationService = authorizationService;
+    }
+
+    public async Task Handle(Query request, CancellationToken cancellationToken)
+    {
+        await _authorizationService.AuthorizeAdminAsync(request.Username, cancellationToken);
+    }
+}
