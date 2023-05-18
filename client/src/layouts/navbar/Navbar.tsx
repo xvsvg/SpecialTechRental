90053e2d64e7885e59196fd8751f61b4f7dd31d6
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-19 02:06:47 +0300

    feat: add navbar

---
+++ client/src/layouts/navbar/Navbar.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@mui/material"
+import "./Navbar.css"
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+	return (
+		<div>
+			<div id="main-navbar" className="navbar">
+				<h2 className="logo">Tech rental</h2>
+				<nav className="panel">
+					<ul>
+					<li>
+							<Button component={Link} to="/">Home</Button>
+						</li>
+						<li>
+							<Button>Profile</Button>
+						</li>
+						<li>
+							<Button component={Link} to="/login">Login</Button>
+						</li>
+						<li>
+							<Button component={Link} to="/register">Register</Button>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	)
+}
+
+export default Navbar
