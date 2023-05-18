b3978d2536a9ba19255f90a48b78a1814c1b3413
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-19 02:09:44 +0300

    feat: add home page

---
+++ client/src/pages/HomePage/Home.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../features"
+import { Content } from "../../layouts"
+
+const HomePage = () => {
+	return (
+		<Header />
+		// {/* <Content /> */ }
+	)
+}
+
+export default HomePage
