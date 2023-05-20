2919a429cdbfb9e63bb7e6cafd3c440645828dcf
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-20 19:02:43 +0300

    feat: export forms

--- client/src/components/forms/index.ts
+++ client/src/components/forms/index.ts
@@ -1,3 +1,5 @@
-export * from "./CreateProductForm"
 export * from "./LoginForm"
 export * from "./RegisterForm"
+export * from "./ProfileForm"
+export * from "./ProductForm"
+export * from "./PurchaseForm"
