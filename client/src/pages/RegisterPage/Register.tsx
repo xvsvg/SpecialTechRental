95cc4233128aa2721f0931158577b889d952b0ea
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-20 19:06:25 +0300

    fix: register page

--- client/src/pages/RegisterPage/Register.tsx
+++ client/src/pages/RegisterPage/Register.tsx
@@ -1,12 +1,13 @@
+import { Box } from "@mui/material";
 import { RegisterForm } from "../../components";
-import { Navbar } from "../../layouts";
+import { Header } from "../../features";
 
 const Register = () => {
 	return (
-		<>
-			<Navbar />
+		<Box bgcolor="#132f4b" minHeight="100vh">
+			<Header />
 			<RegisterForm />
-		</>
+		</Box>
 	)
 }
 
