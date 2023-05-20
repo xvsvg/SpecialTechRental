a91ac9c7918b3b3b677e97c032d277f24dec1173
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-20 19:05:36 +0300

    feat: add notification for login page

--- client/src/pages/LoginPage/Login.tsx
+++ client/src/pages/LoginPage/Login.tsx
@@ -1,12 +1,19 @@
+import { Box } from "@mui/material";
 import { LoginForm } from "../../components";
-import { Navbar } from "../../layouts";
+import { useLocation } from "react-router-dom";
+import { Header, Notification } from "../../features";
 
 const Login = () => {
+	const location = useLocation()
+	const message = location.state && location.state.message
+	const type = location.state && location.state.type
+
 	return (
-		<>
-			<Navbar />
+		<Box bgcolor="#132f4b" minHeight='100vh'>
+			{message && <Notification message={message} type={type} />}
+			<Header />
 			<LoginForm />
-		</>
+		</Box>
 	)
 }
 
