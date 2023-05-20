bba7bc0901d0ec077668b9623afe113f7fa6d9c0
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-20 19:05:18 +0300

    feat: add notification usage for home page

--- client/src/pages/HomePage/Home.tsx
+++ client/src/pages/HomePage/Home.tsx
@@ -1,10 +1,19 @@
-import { Header } from "../../features"
-import { Content } from "../../layouts"
+import { Box } from "@mui/material"
+import { Header, Notification } from "../../features"
+import { useLocation } from "react-router-dom";
+import { Pagination } from "../../components";
 
 const HomePage = () => {
+	const location = useLocation()
+	const message = location.state && location.state.message
+	const type = location.state && location.state.type
+
 	return (
-		<Header />
-		// {/* <Content /> */ }
+		<Box bgcolor="#001e3c" minHeight='100vh' >
+			{message && <Notification message={message} type={type} />}
+			<Header />
+			<Pagination apiUrl={"https://jsonplaceholder.typicode.com/photos"} />
+		</Box>
 	)
 }
 
