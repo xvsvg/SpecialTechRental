757e1ec639c6d64dec3033edd28d1459f388ea05
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-24 14:43:07 +0300

    feat: add api for profile

--- client/src/pages/ProfilePage/Profile.tsx
+++ client/src/pages/ProfilePage/Profile.tsx
@@ -1,39 +1,63 @@
 import { Box } from "@mui/material"
-import { ItemTable, ProfileForm } from "../../components"
-import { Item } from "../../components/tables/ItemTable";
+import { CreateProductForm, ItemTable, ProfileForm } from "../../components"
 import { useEffect, useState } from "react";
-import { getUser } from "../../lib/users/users";
+import { getAllUsers } from "../../lib/users/users";
 import { getCookie } from "typescript-cookie";
-import { IUser } from "../../shared";
+import { IProductPage, IUserPage } from "../../shared";
 import { Notification } from "../../features";
 import { useLocation } from "react-router-dom";
-
-const initial: Item[] = [
-	{ id: 1, name: "Item 1", quantity: 10 },
-	{ id: 2, name: "Item 2", quantity: 5 },
-	{ id: 3, name: "Item 3", quantity: 8 },
-];
+import { getIdentityUser } from "../../lib/identity/identity";
+import { getAllProducts } from "../../lib/products/products";
 
 const Profile = () => {
-	const [items, setItems] = useState(initial);
-	const [error, setError] = useState<string | null>(null)
-	const [user, setUser] = useState<IUser>({ id: '', firstName: 'first name', middleName: 'middle name', lastName: 'last name', birthDate: 'yy-mm-dd', number: 'phone number', image: '', money: 0, orders: [] })
+	const [error, setError] = useState<string | null>(null);
+	const [isAdmin, setIsAdmin] = useState(false);
+	const [products, setProducts] = useState<IProductPage | null>();
 
-	const location = useLocation()
-	const message = location.state && location.state.message
-	const type = location.state && location.state.type
+	const location = useLocation();
+	const message = location.state && location.state.message;
+	const type = location.state && location.state.type;
+
+	useEffect(() => {
+		getUser();
+		fetchItems();
+	}, []); // Пустой массив зависимостей, чтобы эффекты выполнялись только при монтировании компонента
+
+	const getUser = async () => {
+		try {
+			const { data } = await getIdentityUser(
+				getCookie("jwt-authorization") ?? "",
+				getCookie("current-user") ?? ""
+			);
+			if (data.role === "admin") {
+				setIsAdmin(true);
+			}
+		}catch(error){
+		}
+	};
+
+	const fetchItems = () => {
+			(async () => {
+				try {
+					const response = await getAllProducts();
+					setProducts(response.data);
+				} catch (error) {
+					setProducts(null)
+				}
+			})();
+	};
 
 	return (
 		<Box bgcolor="#132f4b" display={"flex"} alignItems={"center"} height={"100vh"} width={"100vw"}>
-			{error && <Notification message={error} type='error' />}
+			{error && <Notification message={error} type="error" />}
 			{message && <Notification message={message} type={type} />}
 			<Box display={"flex"} justifyContent={"space-evenly"} width={"100%"}>
-				<ProfileForm
-					setError={setError} />
-				<ItemTable items={items} setItems={setItems} />
+				<ProfileForm setError={setError} />
+				<ItemTable products={products ?? { orders: [], page: 0, totalPage: 0 }} />
+				{isAdmin && <CreateProductForm />}
 			</Box>
 		</Box>
-	)
-}
+	);
+};
 
-export default Profile
+export default Profile;
