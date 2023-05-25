80fd1279b683d09e0ab38264c7810ed617bb10bd
疯 狂的冲刺 <pobedotriples@gmail.com>
2023-05-25 16:14:24 +0300

    feat: add docker client

--- client/src/components/forms/PurchaseForm/PurchaseForm.tsx
+++ client/src/components/forms/PurchaseForm/PurchaseForm.tsx
@@ -3,115 +3,134 @@ import { useState } from "react";
 import { Add, Remove } from "@mui/icons-material";
 import { green } from "@mui/material/colors";
 import { Report } from "../../reports";
+import { PDFDocument } from "pdf-lib";
 
-interface PurchaseProps{
-	total: number
-	onSubmit: () => void
+interface PurchaseProps {
+  total: number;
+  onSubmit: () => void;
 }
 
-const PurchaseForm = ({total, onSubmit} : PurchaseProps) => {
-	const [quantity, setQuantity] = useState(1);
-	const [showReport, setShowReport] = useState(false)
+const PurchaseForm = ({ total, onSubmit }: PurchaseProps) => {
+  const [quantity, setQuantity] = useState(1);
+  const [showReport, setShowReport] = useState(false);
+  const [purchasedItem, setPurchasedItem] = useState("");
 
-	const handleIncreaseQuantity = () => {
-		setQuantity((prevQuantity) => prevQuantity + 1);
-	};
+  const handleIncreaseQuantity = () => {
+    setQuantity((prevQuantity) => prevQuantity + 1);
+  };
 
-	const handleDecreaseQuantity = () => {
-		if (quantity > 1) {
-			setQuantity((prevQuantity) => prevQuantity - 1);
-		}
-	};
+  const handleDecreaseQuantity = () => {
+    if (quantity > 1) {
+      setQuantity((prevQuantity) => prevQuantity - 1);
+    }
+  };
 
-	const handleQuantityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		const newQuantity = parseInt(event.target.value);
-		setQuantity(newQuantity || 0);
-	};
+  const handleQuantityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newQuantity = parseInt(event.target.value);
+    setQuantity(newQuantity || 0);
+  };
 
-	const calculateCost = (price: number, quantity: number) => {
-		return price * quantity;
-	};
+  const calculateCost = (price: number, quantity: number) => {
+    return price * quantity;
+  };
 
-	const handleSubmit = (event: React.FormEvent) => {
-		event.preventDefault()
-		onSubmit()
-		setShowReport(true)
-	};
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    onSubmit();
+    setShowReport(true);
+  };
 
-	return (
-		<Box sx={{
-			p: 2,
-			display: "flex",
-			flexDirection: "column",
-			alignItems: "center",
-			justifyContent: "center",
-			backgroundColor: "#132f4b",
-			maxWidth: '400px',
-			margin: '0 auto',
-			marginTop: '100px',
-			borderRadius: 3
-		}}>
-			<Typography variant="h6" align="center" color='white'>
-				Purchase Form
-			</Typography>
-			<FormControl component="form" onSubmit={handleSubmit}>
-				<Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
-					<IconButton onClick={handleDecreaseQuantity}>
-						<Remove />
-					</IconButton>
-					<TextField
-						id="quantity"
-						variant="standard"
-						type="text"
-						value={quantity}
-						onChange={handleQuantityChange}
-						InputProps={{
-							style: {
-								color: "white",
-							},
-						}}
-						sx={{
-							width: "70px",
-							textAlign: "center",
-							"& .MuiInputBase-root": {
-								color: "white",
-								"&:before": {
-									borderBottomColor: "white",
-								},
-								"&:hover:before": {
-									borderBottomColor: "white",
-								},
-								"&.Mui-focused:before": {
-									borderBottomColor: "green",
-								},
-								"&.Mui-focused:after": {
-									borderBottomColor: "green",
-								},
-							},
-						}}
-					/>
-					<IconButton onClick={handleIncreaseQuantity}>
-						<Add />
-					</IconButton>
-				</Box>
-				<Typography color='white'>Total price: {calculateCost(total, quantity)}</Typography>
-				<Button
-					variant="contained"
-					color="primary"
-					type="submit"
-					onClick={handleSubmit}
-					sx={{
-						backgroundColor: 'inherit', "&:hover": {
-							backgroundColor: green[500],
-							color: "white",
-						}
-					}}>
-					Submit
-				</Button>
-				{showReport && <Report />}
-			</FormControl>
-		</Box>
-	);
+  const handleDownloadPDF = async () => {
+    const pdfDoc = await generatePDF();
+
+    const pdfBytes = await pdfDoc.save();
+    const blob = new Blob([pdfBytes], { type: "application/pdf" });
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "invoice.pdf";
+    link.click();
+
+    URL.revokeObjectURL(url);
+  };
+
+  const generatePDF = async () => {
+    const pdfDoc = await PDFDocument.create();
+    const page = pdfDoc.addPage();
+    page.drawText(`Purchased Item: ${purchasedItem}`, { x: 50, y: 50 });
+
+    return pdfDoc;
+  };
+
+  return (
+    <Box sx={{ p: 2, display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", backgroundColor: "#132f4b", maxWidth: '400px', margin: '0 auto', marginTop: '100px', borderRadius: 3 }}>
+      <Typography variant="h6" align="center" color="white">
+        Purchase Form
+      </Typography>
+      <FormControl component="form" onSubmit={handleSubmit}>
+        <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+          <IconButton onClick={handleDecreaseQuantity}>
+            <Remove />
+          </IconButton>
+          <TextField
+            id="quantity"
+            variant="standard"
+            type="text"
+            value={quantity}
+            onChange={handleQuantityChange}
+            InputProps={{
+              style: {
+                color: "white",
+              },
+            }}
+            sx={{
+              width: "70px",
+              textAlign: "center",
+              "& .MuiInputBase-root": {
+                color: "white",
+                "&:before": {
+                  borderBottomColor: "white",
+                },
+                "&:hover:before": {
+                  borderBottomColor: "white",
+                },
+                "&.Mui-focused:before": {
+                  borderBottomColor: "green",
+                },
+                "&.Mui-focused:after": {
+                  borderBottomColor: "green",
+                },
+              },
+            }}
+          />
+          <IconButton onClick={handleIncreaseQuantity}>
+            <Add />
+          </IconButton>
+        </Box>
+        <Typography color="white">Total price: {calculateCost(total, quantity)}</Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          onClick={handleSubmit}
+          sx={{
+            backgroundColor: 'inherit', "&:hover": {
+              backgroundColor: green[500],
+              color: "white",
+            }
+          }}
+        >
+          Submit
+        </Button>
+        {showReport && (
+          <Report
+            onDownloadPDF={handleDownloadPDF}
+          />
+        )}
+      </FormControl>
+    </Box>
+  );
 };
 
 export default PurchaseForm;
--- client/src/components/reports/Report.tsx
+++ client/src/components/reports/Report.tsx
@@ -1,42 +1,14 @@
 import { Box, Typography } from "@mui/material";
-import { PDFDocument } from "pdf-lib";
 
-const downloadPDF = async () => {
-  // Create a new PDF document
-  const pdfDoc = await PDFDocument.create();
+interface ReportProps {
+  onDownloadPDF: () => void;
+}
 
-  // Add a blank page to the document
-  const page = pdfDoc.addPage();
-
-  // Draw some text on the page
-  page.drawText("Sample PDF Content", { x: 50, y: 50 });
-
-  // Serialize the PDF document to a Uint8Array
-  const pdfBytes = await pdfDoc.save();
-
-  // Create a Blob object from the PDF bytes
-  const blob = new Blob([pdfBytes], { type: "application/pdf" });
-
-  // Generate a URL for the Blob object
-  const url = URL.createObjectURL(blob);
-
-  // Create a temporary link element
-  const link = document.createElement("a");
-  link.href = url;
-  link.download = "document.pdf";
-
-  // Programmatically trigger the download
-  link.click();
-
-  // Clean up the URL and link element
-  URL.revokeObjectURL(url);
-};
-
-const ExampleComponent = () => {
+const Report = ({ onDownloadPDF }: ReportProps) => {
   return (
     <Box>
       <Typography>
-        <a href="/report" onClick={downloadPDF}>
+        <a href="/report" onClick={onDownloadPDF}>
           Download PDF
         </a>
       </Typography>
@@ -44,4 +16,4 @@ const ExampleComponent = () => {
   );
 };
 
-export default ExampleComponent;
+export default Report;
